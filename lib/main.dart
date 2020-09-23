995e9a54b62054ca302384c71415864ec444cdf8
alexRai98 <raikcuerda19@gmail.com>
2020-09-23 01:54:14 -0500

    build fisrt strucure and first landing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:challengeChat/views/signin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,64 +15,40 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello world:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello world")),
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                child: Image(
+                  image: AssetImage("assets/images/bitacora.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: double.infinity,
+                  height: 80,
+                  color: Colors.black.withOpacity(0.5),
+                  child: Center(
+                    child: Text(
+                      'Hello Rai',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget appBarMain(BuildContext context){
+  return AppBar(
+    title: Image.asset("assets/images/imagetest.svg"),
+  );
+}
